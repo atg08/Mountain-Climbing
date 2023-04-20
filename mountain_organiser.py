3b4fcdb4ded6fe1cb35723fad9b23c48df49f4bc
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-20 23:45:48 +0800

    Update mountain_organiser.py
    Task 5, last fixed.
    
    Coded KeyError.

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -9,10 +9,11 @@ class MountainOrganiser:
         self.merged_list = []
 
     def cur_position(self, mountain: Mountain) -> int:
-        for i in range(len(self.merged_list)):
-            if self.merged_list[i] == mountain:
-                return i
-            
+            for i in range(len(self.merged_list)):
+                if self.merged_list[i].name == mountain.name:
+                    return i
+            raise KeyError()
+
     def add_mountains(self, mountains: list[Mountain]) -> None:
 
         if self.merged_list == []:
