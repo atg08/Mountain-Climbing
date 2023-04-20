69bf7de3cdf36f1e960599dabb38c567ef372257
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-20 23:05:25 +0800

    Update mountain_organiser.py
    task 4 start. haven't done

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 
 from mountain import Mountain
+from algorithms.mergesort import *
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.merged_list = []
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
-
+        for item in self.merged_list:
+            if item == mountain:
+                return self.merged_list.index(item)
+            
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+
+        if self.merged_list == []:
+            self.merged_list = mountains
+
+        else:
+            self.merged_list = merge(self.merged_list,mountains)
