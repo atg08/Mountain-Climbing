bb5c33f397a370bd071c884c1fe986d3a4a81479
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-20 23:20:11 +0800

    Update mountain.py
    Task 5, mountain.py updated.
    
    No more magic method

--- mountain.py
+++ mountain.py
@@ -7,14 +7,3 @@ class Mountain:
     name: str
     difficulty_level: int
     length: int
-
-    def __eq__(self, other):
-        return self.length == other.length
-
-    def __lt__(self, other):
-        return self.length < other.length
-
-    def __le__(self, other):
-        if self.__eq__(other):
-          return self.name < other.name
-        return self.__lt__(other)
