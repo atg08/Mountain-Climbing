f9ba061cba79a600dcbd809e6f91a25967dcacfc
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-22 00:02:49 +0800

    Update mountain_manager.py
    Task 6, 5/6 Done.
    
    def group_by_difficulty() left.

--- mountain_manager.py
+++ mountain_manager.py
@@ -1,5 +1,5 @@
 from mountain import Mountain
-import mountain_organiser
+from algorithms import mergesort
 
 class MountainManager:
 
@@ -9,7 +9,6 @@ class MountainManager:
 
     def add_mountain(self, mountain: Mountain):
         # Add a mountain to the manager
-
         self.manager_list.append(mountain)
         return self.manager_list
 
@@ -23,16 +22,16 @@ class MountainManager:
          for i in range(self.manager_list):
             if self.manager_list[i] == old:
                 self.manager_list[i] = new
-                
 
     def mountains_with_difficulty(self, diff: int):
         # Return a list of all mountains with this difficulty.
+        self.difficulty_list = []
         for i in range(len(self.manager_list)):
-            self.difficulty_list.append((self.manager_list[i],diff))
-
+            if self.manager_list[i].difficulty_level == diff:
+                self.difficulty_list.append((self.manager_list[i]))
+    
         return self.difficulty_list
 
     def group_by_difficulty(self):
-        # Returns a list of lists of all mountains, 
-        # grouped by and sorted by ascending difficulty.
-        raise NotImplementedError()
+        # Returns a list of lists of all mountains, grouped by and sorted by ascending difficulty.
+        raise NotImplementedError
