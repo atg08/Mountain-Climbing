9273e0b2a33bbfb9547c0055e9ca641a6c9e92b8
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-22 12:43:07 +0800

    Update mergesort.py
    mergesort for Task 6 function added.

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -38,6 +38,32 @@ def merge(l1: list[T], l2: list[T], key=lambda x:x) -> list[T]:
     new_list += l2[cur_right:]
     return new_list
 
+def mergediff(l1: list[T], l2: list[T], key=lambda x:x) -> list[T]:
+    new_list = []
+    cur_left = 0
+    cur_right = 0
+    l1 = mergesortdiff(l1)
+    l2 = mergesortdiff(l2)
+    while cur_left < len(l1) and cur_right < len(l2):
+        if key(l1[cur_left].difficulty_level) < key(l2[cur_right].difficulty_level):
+            new_list.append(l1[cur_left])
+            cur_left += 1
+        elif key(l1[cur_left].difficulty_level) > key(l2[cur_right].difficulty_level):
+            new_list.append(l2[cur_right])
+            cur_right += 1
+        else:
+            if l1[cur_left].name < l2[cur_right].name:
+               new_list.append(l1[cur_left])
+               cur_left += 1
+            else:
+               new_list.append(l2[cur_right])
+               cur_right += 1
+                
+    new_list += l1[cur_left:]
+    new_list += l2[cur_right:]
+
+    return new_list
+
 def mergesort(l: list[T]) -> list[T]:
     """
     Sort a list using the mergesort operation.
@@ -49,3 +75,15 @@ def mergesort(l: list[T]) -> list[T]:
     l1 = mergesort(l[:break_index])
     l2 = mergesort(l[break_index:])
     return merge(l1, l2)
+
+def mergesortdiff(l: list[T]) -> list[T]:
+    """
+    Sort a list using the mergesort operation.
+    :complexity: Best/Worst Case O(NlogN * comp(T))
+    """
+    if len(l) <= 1:
+        return l
+    break_index = (len(l)+1) // 2
+    l1 = mergesortdiff(l[:break_index])
+    l2 = mergesortdiff(l[break_index:])
+    return mergediff(l1, l2)
