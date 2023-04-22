1398b5c7807d99a3bfe4b9333c2e8ae3f7b3b8d0
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-22 13:50:15 +0800

    Update mountain_manager.py
    Task 6 Done.

--- mountain_manager.py
+++ mountain_manager.py
@@ -14,16 +14,19 @@ class MountainManager:
 
     def remove_mountain(self, mountain: Mountain):
         # Remove a mountain from the manager
-        # for i in self.manager_list:
-        #     if i == mountain:
-        #         del mountain
-        raise NotImplementedError
+        for inner_list in self.grouped_list:
+            for inner_index in range(len(inner_list)):
+                if inner_list[inner_index] == mountain:
+                    if len(inner_list) == 1:
+                        self.grouped_list.remove(inner_list)
+                    else: 
+                        self.grouped_list[inner_list].pop(inner_index)
 
-    def edit_mountain(self, old: Mountain, new: Mountain):
-         for i in range(self.manager_list):
-            for j in i:
-                if self.manager_list[i][j] == old:
-                    self.manager_list[i][j] = new
+    def edit_mountain(self, old: Mountain, new: Mountain): 
+        for inner_list in self.grouped_list:
+            for inner_index in range(len(inner_list)):
+                if inner_list[inner_index] == old:
+                    inner_list[inner_index] == new
 
     def mountains_with_difficulty(self, diff: int):
         # Return a list of all mountains with this difficulty.
@@ -36,25 +39,22 @@ class MountainManager:
 
     def group_by_difficulty(self):
         # Returns a list of lists of all mountains, grouped by and sorted by ascending difficulty.
-  
+        
+        difficulty_counter = []
         self.manager_list = mergediff(self.manager_list[:len(self.manager_list) // 2],
                                       self.manager_list[len(self.manager_list) // 2:])
-        
-        '''
-            [
-            Mountain(name='m1', difficulty_level=2, length=2),
-            Mountain(name='m2', difficulty_level=2, length=9),
-            Mountain(name='m3', difficulty_level=3, length=6),
-            Mountain(name='m4', difficulty_level=3, length=1), 
-            Mountain(name='m5', difficulty_level=4, length=6), 
-            Mountain(name='m6', difficulty_level=7, length=3), 
-            Mountain(name='m7', difficulty_level=7, length=7), 
-            Mountain(name='m8', difficulty_level=7, length=8), 
-            Mountain(name='m9', difficulty_level=7, length=6)
-            ]
-        '''
-        
-        
+    
+        for i in range(len(self.manager_list)):
+            if self.manager_list[i].difficulty_level not in difficulty_counter:
+                difficulty_counter.append(self.manager_list[i].difficulty_level)
 
+        for j in difficulty_counter: 
+            sub_list = []
+            for i in range(len(self.manager_list)):
+                if self.manager_list[i].difficulty_level == j:
+                    sub_list.append(self.manager_list[i])
+            self.grouped_list.append(sub_list)
 
-
+        self.manager_list = []
+        return self.grouped_list
+       
