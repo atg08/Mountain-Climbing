1810237432b0be1a47bd71a1640f1c118cccd985
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-23 13:28:24 +0800

    Task 6 Done
    Task 6 Done

--- mountain_manager.py
+++ mountain_manager.py
@@ -13,23 +13,20 @@ class MountainManager:
         self.manager_list.append(mountain)
 
     def remove_mountain(self, mountain: Mountain):
-        # Remove a mountain from the manager
         for inner_list in self.grouped_list:
-            for inner_index in range(len(inner_list)):
-                if inner_list[inner_index] == mountain:
-                    if len(inner_list) == 1:
-                        self.grouped_list.remove(inner_list)
-                    else: 
-                        self.grouped_list[inner_list].pop(inner_index)
+            if mountain in inner_list:
+                inner_list.remove(mountain)
+                if not inner_list:
+                    self.grouped_list.remove(inner_list)
 
-    def edit_mountain(self, old: Mountain, new: Mountain): 
+    def edit_mountain(self, old: Mountain, new: Mountain):
         for inner_list in self.grouped_list:
-            for inner_index in range(len(inner_list)):
-                if inner_list[inner_index] == old:
-                    inner_list[inner_index] == new
+            if old in inner_list:
+                inner_list[inner_list.index(old)] = new
 
     def mountains_with_difficulty(self, diff: int):
         # Return a list of all mountains with this difficulty.
+
         self.difficulty_list = [index for index in self.manager_list if index.difficulty_level == diff]
         return self.difficulty_list
     
