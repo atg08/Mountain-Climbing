23baedfd66661a4634de8f8e98f8c565c07e30d5
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-26 01:16:25 +0800

    Task 5 done

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -20,14 +20,14 @@ def merge(l1: list[T], l2: list[T], key=lambda x:x) -> list[T]:
     l1 = mergesort(l1)
     l2 = mergesort(l2)
     while cur_left < len(l1) and cur_right < len(l2):
-        if key(l1[cur_left].length) < key(l2[cur_right].length):
+        if key(l1[cur_left]) < key(l2[cur_right]):
             new_list.append(l1[cur_left])
             cur_left += 1
-        elif key(l1[cur_left].length) > key(l2[cur_right].length):
+        elif key(l1[cur_left]) > key(l2[cur_right]):
             new_list.append(l2[cur_right])
             cur_right += 1
         else:
-            if l1[cur_left].name < l2[cur_right].name:
+            if l1[cur_left] < l2[cur_right]:
                new_list.append(l1[cur_left])
                cur_left += 1
             else:
@@ -38,33 +38,7 @@ def merge(l1: list[T], l2: list[T], key=lambda x:x) -> list[T]:
     new_list += l2[cur_right:]
     return new_list
 
-def mergediff(l1: list[T], l2: list[T], key=lambda x:x) -> list[T]:
-    new_list = []
-    cur_left = 0
-    cur_right = 0
-    l1 = mergesortdiff(l1)
-    l2 = mergesortdiff(l2)
-    while cur_left < len(l1) and cur_right < len(l2):
-        if key(l1[cur_left].difficulty_level) < key(l2[cur_right].difficulty_level):
-            new_list.append(l1[cur_left])
-            cur_left += 1
-        elif key(l1[cur_left].difficulty_level) > key(l2[cur_right].difficulty_level):
-            new_list.append(l2[cur_right])
-            cur_right += 1
-        else:
-            if l1[cur_left].name < l2[cur_right].name:
-               new_list.append(l1[cur_left])
-               cur_left += 1
-            else:
-               new_list.append(l2[cur_right])
-               cur_right += 1
-                
-    new_list += l1[cur_left:]
-    new_list += l2[cur_right:]
-
-    return new_list
-
-def mergesort(l: list[T]) -> list[T]:
+def mergesort(l: list[T],key=lambda x:X) -> list[T]:
     """
     Sort a list using the mergesort operation.
     :complexity: Best/Worst Case O(NlogN * comp(T))
@@ -72,18 +46,7 @@ def mergesort(l: list[T]) -> list[T]:
     if len(l) <= 1:
         return l
     break_index = (len(l)+1) // 2
-    l1 = mergesort(l[:break_index])
-    l2 = mergesort(l[break_index:])
-    return merge(l1, l2)
+    l1 = mergesort(l[:break_index], key = key)
+    l2 = mergesort(l[break_index:], key = key)
+    return merge(l1, l2, key = key)
 
-def mergesortdiff(l: list[T]) -> list[T]:
-    """
-    Sort a list using the mergesort operation.
-    :complexity: Best/Worst Case O(NlogN * comp(T))
-    """
-    if len(l) <= 1:
-        return l
-    break_index = (len(l)+1) // 2
-    l1 = mergesortdiff(l[:break_index])
-    l2 = mergesortdiff(l[break_index:])
-    return mergediff(l1, l2)
