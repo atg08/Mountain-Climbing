01eecb2c6eaf6589c17ac70d96cf9442ff2e3b25
junemonash <126083693+junemonash@users.noreply.github.com>
2023-04-26 01:17:17 +0800

    task 6 done

--- mountain_manager.py
+++ mountain_manager.py
@@ -1,61 +1,52 @@
 from mountain import Mountain
-from algorithms.mergesort import *
+from algorithms.binary_search import *
 
 class MountainManager:
 
     def __init__(self) -> None:
-        self.manager_list = []      # for add mountain
-        self.difficulty_list = []   # for checking difficulty
-        self.grouped_list = []      # for return confirm list
+        self.mountain_list : list[Mountain] = []
 
     def add_mountain(self, mountain: Mountain):
         # Add a mountain to the manager
-        self.manager_list.append(mountain)
+        mountain_index = binary_search(l = self.mountain_list, item = mountain, key = lambda x : (x.difficulty_level, x.name), is_insert = True)
+
+        self.mountain_list.insert(mountain_index,mountain)
 
     def remove_mountain(self, mountain: Mountain): 
         # Remove a mountain from the manager 
         
-        for inner_list in self.grouped_list: 
-            for inner_index in range(len(inner_list)): 
-                if inner_list[inner_index] == mountain: 
-                    if len(inner_list) == 1: 
-                        self.grouped_list.remove(inner_list) 
-                    else: 
-                        self.grouped_list[inner_list].pop(inner_index)
+        mountain_index = binary_search(l = self.mountain_list, item = mountain, key = lambda x: (x.difficulty_level, x.name))
+
+        del self.mountain_list[mountain_index]
 
     def edit_mountain(self, old: Mountain, new: Mountain): 
 
-        for inner_list in self.grouped_list: 
-
-            for inner_index in range(len(inner_list)): 
-                if inner_list[inner_index] == old: inner_list[inner_index] == new
+        self.remove_mountain(mountain = old)
+        self.add_mountain(mountain = new)
 
     def mountains_with_difficulty(self, diff: int):
         # Return a list of all mountains with this difficulty.
 
-        self.difficulty_list = [index for index in self.manager_list if index.difficulty_level == diff]
-        return self.difficulty_list
+        same_diff_mountain : list[Mountain] = []
+
+        for diff_index in range(len(self.mountain_list)):
+            if self.mountain_list[diff_index].difficulty_level == diff:
+                same_diff_mountain.append(self.mountain_list[diff_index])
+
+        return same_diff_mountain
     
     def group_by_difficulty(self):
         # Returns a list of lists of all mountains, grouped by and sorted by ascending difficulty.
         
-        difficulty_counter = []
-        
-        self.manager_list = mergediff(self.manager_list[:len(self.manager_list) // 2],
-                                      self.manager_list[len(self.manager_list) // 2:])
+        index_diff = 0
+        grouped_list_diff : list[list[Mountain]] = []
+
+        while (index_diff < len(self.mountain_list)):
+            
+            grouped_list : list[Mountain] = self.mountains_with_difficulty(self.mountain_list[index_diff].difficulty_level)
+            index_diff += len(grouped_list)
     
-        for i in range(len(self.manager_list)):
-            if self.manager_list[i].difficulty_level not in difficulty_counter:
-                difficulty_counter.append(self.manager_list[i].difficulty_level)
-
-        for j in difficulty_counter: 
-            sub_list = []
-            for i in range(len(self.manager_list)):
-                if self.manager_list[i].difficulty_level == j:
-                    sub_list.append(self.manager_list[i])
-            self.grouped_list.append(sub_list)
-
-        self.manager_list = []
+            grouped_list_diff.append(grouped_list)
         
-        return self.grouped_list
+        return grouped_list_diff
        
