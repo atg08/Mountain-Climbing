466c6906d69b587a4c8f432cca7276ffd2e7fce7
junemonash <126083693+junemonash@users.noreply.github.com>
2023-05-05 13:45:39 +0800

    Update mountain_organiser.py

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -10,11 +10,21 @@ class MountainOrganiser:
         self.merged_list = []
 
     def cur_position(self, mountain: Mountain) -> int:
+        '''
+        Finds the rank of the provided mountain given all mountains included so far. 
+        See below for an example. 
+        
+        Raises KeyError if this mountain hasn't been added yet.
+        '''
         
         rank = binary_search(l = self.merged_list,item = mountain, key= lambda a : (a.length, a.name))
         
         return rank
             
     def add_mountains(self, mountains: list[Mountain]) -> None:
+
+        '''
+        Adds a list of mountains to the organiser
+        '''
 
         self.merged_list = mergesort(l = self.merged_list+ mountains, key= lambda a : (a.length, a.name))
